8ae4cdf78b25614298815d5e70d06b661b90a7a6
mr.b <bruno932151@gmail.com>
2022-04-02 13:02:01 -0500

    main was created

---
+++ monty.h
@@ -0,0 +1,73 @@
+#ifndef _MONTY_
+#define _MONTY_
+
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <ctype.h>
+
+
+extern FILE *fd;
+/**
+ * struct variable_s - global variables
+ * @int: line number
+ * @fd: pointer to file
+ * 
+ * Description: variables global
+ */
+
+typedef struct variable_s
+{
+	int nl;
+	FILE *fd;
+        char **tokens;
+        char *buffer;
+        int value;
+} variable_t;
+
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+        int n;
+        struct stack_s *prev;
+        struct stack_s *next;
+} stack_node;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+        char *opcode;
+        void (*f)(stack_node **stack, unsigned int line_number);
+} instruction_t;
+
+void (*matcher(variable_t *gb_var))(stack_node **stack, unsigned int line_number);
+/*function handler*/
+void push(stack_node **stack, unsigned int line_number);
+void pall(stack_node **stack, unsigned int line_number);
+
+int sdig(char *s);
+
+#endif
